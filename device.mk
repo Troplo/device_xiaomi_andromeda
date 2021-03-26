80f313856aaa9c9c1ac0402fb8bf1294c4298aec
Nicholas Magill <nmagill1979@gmail.com>
2021-03-26 22:19:11 +0000

    andromeda: parts: Add slider sound settings

--- device.mk
+++ device.mk
@@ -126,6 +126,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/parts/snd,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
