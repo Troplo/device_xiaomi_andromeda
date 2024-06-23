58b142434e7111bd41e513475aa3cd604e94f9be
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    andromeda: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Inherit vendor
 $(call inherit-product, vendor/xiaomi/andromeda/andromeda-vendor.mk)
 
